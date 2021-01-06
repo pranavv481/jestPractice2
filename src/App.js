fb1cdb48f72b5f9bb66e0b001677a08ac408834b
pranavv481 <pranavv481@gmail.com>
2021-01-06 18:08:12 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [count, setCount]= useState(0);
   return (
     <div data-test="component-app">
-      
+      <h1 data-test="counter-display">this counter is currently 
+      <span data-test="count">{count}</span>
+      </h1>
+      <button data-test="increment-button" onClick ={()=>setCount(count+1)}>Incre ment Counter</button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,27 +1,45 @@
+import React from 'react';
 import App from './App';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 Enzyme.configure({ adapter: new Adapter() });
 
+const setup = () => shallow(<App/>)
+const findByTestAttr = (wrapper, val)=>wrapper.find(`[data-test='${val}']`)
+
 test('render without error', () => {
-const wrapper = shallow(<App/>);
-const appComponent = wrapper.find("[data-test='component-app']")
+const wrapper = setup()
+const appComponent = findByTestAttr(wrapper,"component-app")
 expect(appComponent.length).toBe(1)
 })
 
 test('render button', () => {
-
+    const wrapper = setup()
+    const button = findByTestAttr(wrapper,"increment-button")
+    expect(button.length).toBe(1)
 })
 
 test('render counter display', () => {
-
+    const wrapper = setup()
+    const counterDisplay = findByTestAttr(wrapper,"counter-display")
+    expect(counterDisplay.length).toBe(1)
 })
 
 test('counter start at 0', ()=>{
-
+  const wrapper = setup()
+  const count = findByTestAttr(wrapper,"count").text()
+  expect(count).toBe("0")
 })
 
 test('clicking on button increment counter display', ()=>{
+    const wrapper = setup()
+    const button = findByTestAttr(wrapper,"increment-button")
+    //find the button 
+    button.simulate('click');
+    // click the button 
+    const count = findByTestAttr(wrapper,"count").text()
+    expect(count).toBe("1");
+    // find the display and test that number have been incremented  
 
 })
