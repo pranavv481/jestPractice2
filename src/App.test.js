b64584d21280f50014fda86bce53d5163c5eae9d
pranavv481 <pranavv481@gmail.com>
2021-01-06 11:17:28 +0530

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+Enzyme.configure({ adapter: new Adapter() });
+
+test('render without error', () => {
+const wrapper = shallow(<App/>);
+const appComponent = wrapper.find("[data-test='component-app']")
+expect(appComponent.length).toBe(1)
+})
+
+test('render button', () => {
+
+})
+
+test('render counter display', () => {
+
+})
+
+test('counter start at 0', ()=>{
+
+})
+
+test('clicking on button increment counter display', ()=>{
+
+})
